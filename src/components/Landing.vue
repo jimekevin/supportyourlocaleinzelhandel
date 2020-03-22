8e17bbb3c85c694b9bffbbadccc242d9b73bd2ea
Erik Roemmelt <mail@erikroemmelt.com>
2020-03-22 13:04:31 +0100

    Update Landing

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,35 +1,33 @@
 <template>
-<v-ons-page>
-    <div class="content">
-      <v-ons-col>
-        <div class="center">
-          <v-ons-list>
-            <v-ons-list-item>
-              <p>
-                Schön, dass du hier bist! 
-              </p>
-            </v-ons-list-item>
-            <v-ons-list-item>
-                <v-ons-input placeholder="Input your name" float v-model="name"></v-ons-input>
-            </v-ons-list-item>
-            <v-ons-list-item>
-                <router-link to="explore"><v-ons-button class="btn-continue" >OK</v-ons-button></router-link>
-            </v-ons-list-item>
-          </v-ons-list>
-        </div>
-            <!-- <Categories v-on:category-locations="filterLocationsByCategory" /> -->
-
-      </v-ons-col>
-
+  <v-ons-page>
+    <div class="content center background">
+      <!-- Logo -->
+      <div class="logo-container">
+        <img class="logo-img" src="@/assets/imgs/Herz_Logo-05.png">
+      
+        <p class="logo-caption">DIREKT ZU DIR</p>
       </div>
 
-      
+      <!-- Button: Allow Location -->
+      <div>
+        <router-link to="explorer"><v-ons-button class="btn-continue" >STANDORT FREIGEBEN</v-ons-button></router-link>
+      </div>
 
-    </v-ons-page>
+      <!-- Text: oder -->
+      <div class="text">
+        oder
+      </div>
+        
+      <!-- Button: PLZ input -->
+      <div>
+        <router-link to="plz-input"><v-ons-button class="btn-continue" >POSTLEITZAHL EINGEBEN</v-ons-button></router-link>
+      </div>
+    </div>
+  </v-ons-page>
 </template>
 
-
 <script>
+//<v-ons-input placeholder="Input your name" float v-model="name"></v-ons-input>
 
 export default {
   name: "Landing",
@@ -41,13 +39,32 @@ export default {
 };
 </script>
 
-    <style type="text/css" scoped>
+<style type="text/css" scoped>
+  .background {
+    background: #5f6daf !important;
+  }
+  .logo-container {
+    display: inline-block;
+  }
+  .logo-img {
+    display: block;
+    max-width: 80vw;
+    max-height: 40vh;
+    width: auto;
+    height: auto;
+    z-index: 2 !important;
+  }
+  .logo-caption {
+    display: block;
+
+  }
+
   .btn-continue {
-    background-color: #38d786 !important;
+    background-color: #FFFFFF !important;
     margin: auto 0 !important;
     padding: 3px 40px !important;
     font-size: 14px !important;
-    border-radius: 40px !important;
+    border-radius: 4px !important;
     z-index: 2 !important;
   }
-  </style>
+</style>
