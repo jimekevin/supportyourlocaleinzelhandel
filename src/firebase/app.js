aaf1b9a567a0b3611772978caf9dbd7be2249014
Erik Roemmelt <mail@erikroemmelt.com>
2020-03-22 01:40:03 +0100

    Set up Firebase deploy

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -1,11 +1,14 @@
-// import Firebase from 'firebase/app'
-// var firebaseConfig = {
-//     apiKey:
-//     authDomain:
-//     databaseURL:
-//     projectId:
-//     storageBucket:
-//     messagingSenderId:
-//     appId:
-//   };
-// export const App = Firebase.initializeApp(firebaseConfig);
+// Web app Firebase configuration
+import Firebase from 'firebase/app'
+const firebaseConfig = {
+  apiKey: "AIzaSyCUmdfhw4nu7kjHxse_ZZEa8lydbyT2oLM",
+  authDomain: "wirvonhier.firebaseapp.com",
+  databaseURL: "https://wirvonhier.firebaseio.com",
+  projectId: "wirvonhier",
+  storageBucket: "wirvonhier.appspot.com",
+  messagingSenderId: "532669049858",
+  appId: "1:532669049858:web:d332549d1018bd61e6cb1b",
+  measurementId: "G-SL3Y3HM7DW"
+};
+export const App = Firebase.initializeApp(firebaseConfig);
+// export const Analytics = Firebase.analytics();
